c1011bed854d42463305991fe20e820a48182ca4
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-16 21:28:04 -0300

    added stop logic

--- caveman/Assets/Scenes/CharacterMovement.cs
+++ caveman/Assets/Scenes/CharacterMovement.cs
@@ -5,6 +5,8 @@ public class CharacterMovement : MonoBehaviour
 	public float maxSpeed;
 	public float acceleration;
 
+	public float desacceleration;
+
 	Vector2 velocity;
 
 	public Vector2 GetVelocity()
@@ -20,6 +22,21 @@ public class CharacterMovement : MonoBehaviour
 	public void Move(Vector2 desiredDirection)
 	{
 		var newVelocity = velocity + desiredDirection * acceleration * Time.deltaTime;
+
+		if (desiredDirection.sqrMagnitude <= 0) {
+			if (velocity.sqrMagnitude > 0.001f) {
+				Vector2 direction = velocity.normalized;
+				direction.x *= -1;
+
+				newVelocity = velocity + direction * desacceleration * Time.deltaTime;
+
+				if (Mathf.Abs(Mathf.Sign (newVelocity.x) - Mathf.Sign (velocity.x)) < 0.001f) {
+					newVelocity.Set (0, 0);
+				}
+			} else {
+				newVelocity.Set (0, 0);
+			}
+		} 
 
 		if (newVelocity.sqrMagnitude > maxSpeed * maxSpeed) {
 			newVelocity = newVelocity.normalized * maxSpeed;
