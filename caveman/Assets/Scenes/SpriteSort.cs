aa526c1d5b7418311a101e9238130ff9b4265a1d
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-16 22:39:48 -0300

    added more tiles, added sprite sort

---
+++ caveman/Assets/Scenes/SpriteSort.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpriteSort : MonoBehaviour
+{
+	readonly List<SpriteRenderer> sprites = new List<SpriteRenderer>();
+
+	void Awake()
+	{
+		GetComponentsInChildren<SpriteRenderer>(sprites);
+	}
+
+	void LateUpdate()
+	{
+		for (int i = 0; i < sprites.Count; i++) {
+			var sprite = sprites [i];
+			sprite.sortingOrder = Mathf.RoundToInt (transform.position.y * 10f) * -1;
+		}
+	}
+}
