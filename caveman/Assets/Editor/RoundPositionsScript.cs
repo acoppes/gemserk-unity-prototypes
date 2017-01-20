3d32b2285445269533f33d0cc0dc3e6c83cafead
Ariel Coppes <ariel.coppes@gmail.com>
2017-01-19 21:00:34 -0300

    added script to round positions to int

---
+++ caveman/Assets/Editor/RoundPositionsScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class RoundPositionsScript {
+
+	[MenuItem("Extras/Round positions to pixel perfect")]
+	public static void RoundPositions()
+	{
+		GameObject[] gameObjects = UnityEditor.Selection.gameObjects;
+		foreach (var gameObject in gameObjects) {
+			Vector3 position = gameObject.transform.position;
+			gameObject.transform.position = new Vector3 (Mathf.RoundToInt (position.x), Mathf.RoundToInt (position.y), Mathf.RoundToInt (position.z));
+		}
+	}
+}
