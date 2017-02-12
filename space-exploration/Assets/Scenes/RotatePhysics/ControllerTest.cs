112158ec34ee43d8fbea7a2111187f0e3524be12
Ariel Coppes <ariel.coppes@gmail.com>
2017-02-11 22:29:00 -0300

    added scene with test using physics

---
+++ space-exploration/Assets/Scenes/RotatePhysics/ControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ControllerTest : MonoBehaviour {
+
+	public Rigidbody2D body;
+
+	public float accelerateForce = 10.0f;
+
+	public float torqueForce = 1.0f;
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			body.AddRelativeForce (new Vector2 (0, accelerateForce));
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			body.AddTorque (torqueForce, ForceMode2D.Force);
+		} else if (Input.GetKey (KeyCode.D)) {
+			body.AddTorque (-torqueForce, ForceMode2D.Force);
+		}
+	}
+
+}
