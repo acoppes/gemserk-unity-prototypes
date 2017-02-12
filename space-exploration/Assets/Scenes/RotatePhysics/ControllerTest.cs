1c5cd11d0c24f3777cce40961f8f2bbc9b3a7999
Ariel Coppes <ariel.coppes@gmail.com>
2017-02-11 22:37:30 -0300

    added brake and torque with collider

--- space-exploration/Assets/Scenes/RotatePhysics/ControllerTest.cs
+++ space-exploration/Assets/Scenes/RotatePhysics/ControllerTest.cs
@@ -6,12 +6,16 @@ public class ControllerTest : MonoBehaviour {
 
 	public float accelerateForce = 10.0f;
 
+	public float brakeForce = 100.0f;
+
 	public float torqueForce = 1.0f;
 
 	void Update()
 	{
 		if (Input.GetKey (KeyCode.W)) {
 			body.AddRelativeForce (new Vector2 (0, accelerateForce));
+		} else if (Input.GetKey (KeyCode.S)) {
+			body.AddRelativeForce (new Vector2 (0, -brakeForce));
 		}
 
 		if (Input.GetKey (KeyCode.A)) {
