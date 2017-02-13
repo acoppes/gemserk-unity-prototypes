03f7c4ddd398ac85a5a051fe6d5b98dc2ed9f9f1
Ariel Coppes <ariel.coppes@gmail.com>
2017-02-12 22:34:10 -0300

    added quick generator for world

---
+++ space-exploration/Assets/Scenes/RotatePhysics/Planet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	public Sprite[] planets;
+
+	public Transform model;
+
+	public float minSize;
+	public float maxSize;
+
+	public void Randomize()
+	{
+		float scale = UnityEngine.Random.Range (minSize, maxSize);
+
+		model.localScale = new Vector3 (scale, scale, 1.0f);
+
+		var sprite = planets[UnityEngine.Random.Range(0, planets.Length)];
+
+		var renderer = model.GetComponent<SpriteRenderer> ();
+		renderer.sprite = sprite;
+
+		float angle = UnityEngine.Random.Range (0, 360.0f);
+
+		model.transform.localEulerAngles = new Vector3 (0, 0, angle);
+
+		renderer.flipX = UnityEngine.Random.Range (-1.0f, 1.0f) > 0.0f;
+		renderer.flipY = UnityEngine.Random.Range (-1.0f, 1.0f) > 0.0f;
+	}
+}
---
+++ space-exploration/Assets/Scenes/RotatePhysics/WorldGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour {
+
+	public float width;
+	public float height;
+
+	public float planetWidth;
+	public float planetHeight;
+
+	public Planet planetPrefab;
+
+	public float distribution = 0.25f;
+
+	void Awake()
+	{
+		GenerateWorld ();
+	}
+
+	public void GenerateWorld()
+	{
+		float planetSize = planetWidth * planetHeight;
+
+		int total = Mathf.RoundToInt(((width * height) / (planetSize)) * distribution);
+
+		for (int i = 0; i < total; i++) {
+			float x = UnityEngine.Random.Range (-width / 2, width / 2);
+			float y = UnityEngine.Random.Range (-height / 2, height / 2);
+
+			var planet = GameObject.Instantiate (planetPrefab) as Planet;
+			planet.transform.SetParent (this.transform);
+
+			planet.transform.position = new Vector3 (x, y, 0);
+			planet.Randomize();
+		}
+	}
+
+}
