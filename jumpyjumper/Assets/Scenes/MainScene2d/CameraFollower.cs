8ace7a6c1deb6d7d8cba3b3c7dcb328bbef63f1c
Ariel Coppes <ariel.coppes@gmail.com>
2017-05-14 13:12:33 -0300

    added small delay for camera follow

--- jumpyjumper/Assets/Scenes/MainScene2d/CameraFollower.cs
+++ jumpyjumper/Assets/Scenes/MainScene2d/CameraFollower.cs
@@ -3,11 +3,20 @@
 public class CameraFollower : MonoBehaviour {
 
 	public Transform targetTransform;
+
+	public float lerpSpeed = 1.0f;
+
+	public float minDistance = 0.1f;
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		var myposition = transform.position;
-		myposition.y = targetTransform.position.y;
+		myposition.y = Mathf.Lerp(myposition.y, targetTransform.position.y, lerpSpeed * Time.deltaTime);
+
+		if (Mathf.Abs (myposition.y - targetTransform.position.y) < minDistance) {
+			myposition.y = targetTransform.position.y;
+		}
+
 		transform.position = myposition;
 	}
 }
