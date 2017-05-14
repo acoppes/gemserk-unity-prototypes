0cbe1455cee2694f5d28a70f67c2b6fa868cfe73
Ariel Coppes <ariel.coppes@gmail.com>
2017-05-14 13:24:54 -0300

    optional charge jump if pressed before contacts, added mobile impl for jump

--- jumpyjumper/Assets/Scenes/MainScene2d/DragForce.cs
+++ jumpyjumper/Assets/Scenes/MainScene2d/DragForce.cs
@@ -20,60 +20,97 @@ public class DragForce : MonoBehaviour {
 
 	bool chargingJump = true;
 
+	public bool chargeIfJumpingAndTouch = false;
+
 	public Jumper jumper;
+
+	void StartJumpingMode()
+	{
+		chargingJump = jumper.CanJump();
+
+		if (chargingJump) {
+			startPosition = Input.mousePosition;
+			forceIndicator.Show (targetBody.position);
+
+			Time.timeScale = 0.0f;
+		}
+	}
+
+	void UpdateJumpingMode()
+	{
+		if (!chargingJump && chargeIfJumpingAndTouch) {
+			StartJumpingMode ();
+		}
+
+		if (!chargingJump)
+			return;
+
+		Vector2 difference = mainCamera.ScreenToWorldPoint (startPosition) - mainCamera.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 direction = difference.normalized;
+		forceIndicator.UpdateForce (direction, 1.0f);
+	}
+
+	void PerformJump()
+	{
+		if (!chargingJump)
+			return;
+		
+		Time.timeScale = 1.0f;
+
+		var endPosition = Input.mousePosition;
+
+		Vector2 difference = startPosition - endPosition;
+		Vector2 direction = difference.normalized;
+
+		float t = difference.magnitude / maxDistance;
+		float multiplier = impulseCurveMultiplier.Evaluate (t);
+
+		targetBody.velocity = Vector2.zero;
+		targetBody.AddForce (direction * maxImpulse * multiplier, ForceMode2D.Impulse);
+
+		forceIndicator.Hide ();
+	}
+
+	bool IsJumpPressed()
+	{
+		if (Application.isMobilePlatform) {
+			var touchCount = Input.touchCount;
+			if (touchCount == 0)
+				return false;
+
+			var touchPhase = Input.GetTouch (0).phase;
+
+			return (touchPhase == TouchPhase.Moved) || (touchPhase == TouchPhase.Stationary);
+		} else {
+			return Input.GetMouseButton (0);
+		}	
+	}
+
+	bool wasJumpPressed;
 
 	// Update is called once per frame
 	void Update () {
 	
-		if (Input.GetMouseButtonDown (0)) {
+		bool isJumpPresed = IsJumpPressed ();
 
-			chargingJump = jumper.CanJump();
-
-			if (chargingJump) {
-				startPosition = Input.mousePosition;
-				forceIndicator.Show (targetBody.position);
-
-//			forceIndicator.Show ((Vector2) mainCamera.ScreenToWorldPoint (startPosition));
-
-				Time.timeScale = 0.0f;
-			}
-
-		} else if (Input.GetMouseButton (0)) {
-			if (chargingJump) {
-				Vector2 difference = mainCamera.ScreenToWorldPoint(startPosition) - mainCamera.ScreenToWorldPoint(Input.mousePosition);
-
-				Vector2 direction = difference.normalized;
-
-	//			if (difference.magnitude > maxDistance) {
-	//				difference = difference.normalized * maxDistance;
-	//			}
-
-				forceIndicator.UpdateForce (direction, 1.0f);
-
-	//			forceIndicator.UpdateForce (targetBody.position - difference);
-
-	//			forceIndicator.UpdateForce ((Vector2)mainCamera.ScreenToWorldPoint (Input.mousePosition));
-			}
-		} else if (Input.GetMouseButtonUp (0)) {
-
-			if (chargingJump) {
-				Time.timeScale = 1.0f;
-
-				var endPosition = Input.mousePosition;
-
-				Vector2 difference = startPosition - endPosition;
-				Vector2 direction = difference.normalized;
-
-				float t = difference.magnitude / maxDistance;
-				float multiplier = impulseCurveMultiplier.Evaluate (t);
-
-				targetBody.velocity = Vector2.zero;
-				targetBody.AddForce (direction * maxImpulse * multiplier, ForceMode2D.Impulse);
-
-				forceIndicator.Hide ();
-			}
+		if (isJumpPresed && !wasJumpPressed) {
+			StartJumpingMode ();
+		} else if (!isJumpPresed && wasJumpPressed) {
+			PerformJump ();
+		} else {
+			UpdateJumpingMode ();
 		}
 
+		wasJumpPressed = isJumpPresed;
+
+//		if (Input.GetMouseButtonDown (0)) {
+//			StartJumpingMode ();
+//		} else if (Input.GetMouseButton (0)) {
+//			UpdateJumpingMode ();
+//		} else if (Input.GetMouseButtonUp (0)) {
+//			PerformJump ();
+//		}
+//
 		if (Input.GetKeyUp (KeyCode.Space)) {
 			targetBody.velocity = Vector2.zero;
 		}
