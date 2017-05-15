b0873b62401d09b6627a6c676bfda17392986339
Ariel Coppes <ariel.coppes@gmail.com>
2017-05-14 22:17:08 -0300

    added menu to restart game

---
+++ jumpyjumper/Assets/Scenes/MainScene2d/GameMenu.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+	public Button pauseButton;
+
+	public Button restartButton;
+	public Button resumeButton;
+
+	public CanvasGroup menu;
+	public CanvasGroup hud;
+
+	void Start()
+	{
+		ResumeGame ();
+
+		pauseButton.onClick.AddListener (delegate() {
+			PauseGame();
+		});
+
+		restartButton.onClick.AddListener (delegate() {
+			RestartGame();
+		});
+
+		resumeButton.onClick.AddListener (delegate() {
+			ResumeGame();
+		});
+	}
+
+	void PauseGame()
+	{
+		Time.timeScale = 0.0f;
+
+		menu.alpha = 1.0f;
+		menu.interactable = true;
+		menu.blocksRaycasts = true;
+
+		hud.alpha = 0.0f;
+		hud.interactable = false;
+		hud.blocksRaycasts = false;
+	}
+
+	void ResumeGame()
+	{
+		Time.timeScale = 1.0f;
+
+		menu.alpha = 0.0f;
+		menu.interactable = false;
+		menu.blocksRaycasts = false;
+
+		hud.alpha = 1.0f;
+		hud.interactable = true;
+		hud.blocksRaycasts = true;
+	}
+
+	void RestartGame()
+	{
+		Time.timeScale = 1.0f;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+
+}
