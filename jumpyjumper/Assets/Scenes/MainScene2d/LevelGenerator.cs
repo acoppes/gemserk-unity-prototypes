91314b43ad70b88122b4fe7883e147712ee43d25
Ariel Coppes <ariel.coppes@gmail.com>
2017-05-14 22:01:03 -0300

    added basic level generator to test for a while

---
+++ jumpyjumper/Assets/Scenes/MainScene2d/LevelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject platformsParent;
+
+	List<Platform> platforms = new List<Platform>();
+
+	public float minVerticalDistance;
+	public float maxVerticalDistance;
+
+	public int platformsToGenerateCount;
+
+	public float startVerticalPosition;
+
+	public Bounds horizontalBounds;
+
+	// Use this for initialization
+	void Start () {
+
+		platformsParent.GetComponentsInChildren<Platform> (false, platforms);
+
+		Vector2 currentPosition = new Vector2 (0, startVerticalPosition);
+
+		for (int i = 0; i < platformsToGenerateCount; i++) {
+			currentPosition.x = UnityEngine.Random.Range (-horizontalBounds.size.x, horizontalBounds.size.x);
+
+			var platformPrototype = platforms [UnityEngine.Random.Range (0, platforms.Count)];
+			var platformInstance = GameObject.Instantiate (platformPrototype, this.transform);
+			platformInstance.transform.localPosition = currentPosition;
+
+			currentPosition.y += UnityEngine.Random.Range(minVerticalDistance, maxVerticalDistance);
+//			currentHeight += UnityEngine.Random (minVerticalDistance, maxVerticalDistance);
+		}	
+	}
+
+}
