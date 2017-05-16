c71b1cf00bd949c30a32c34fd983b9b02362186a
Ariel Coppes <ariel.coppes@gmail.com>
2017-05-15 21:31:09 -0300

    added curve color for score mark

--- jumpyjumper/Assets/Scenes/MainScene2d/ScoreMark.cs
+++ jumpyjumper/Assets/Scenes/MainScene2d/ScoreMark.cs
@@ -4,8 +4,21 @@ public class ScoreMark : MonoBehaviour {
 
 	public Score score;
 
+	public Jumper jumper;
+
+	public Gradient barColor;
+
+	public SpriteRenderer bar;
+
+	public float maxDistance = 1.0f;
+
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (0, score.GetScore (), 0);
+
+		float distance = transform.position.y - jumper.transform.position.y;
+
+		bar.color = barColor.Evaluate (distance / maxDistance);
+
 	}
 }
