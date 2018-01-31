e3204a4bd130cd7e50e19462faeeee716efe1867
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-30 22:57:46 -0300

    First version of having a fog

--- FogTests/Assets/Scenes/TextureTest.cs
+++ FogTests/Assets/Scenes/TextureTest.cs
@@ -15,8 +15,14 @@ public class TextureTest : MonoBehaviour {
 
 	Texture2D _texture;
 
+	Vision[] _visions;
+
+	public bool testVision = false;
+
     void Start()
     {
+		_visions = FindObjectsOfType<Vision>();
+
 		_texture =  new Texture2D(width, height, TextureFormat.RGBA32, false, true);
 
 		_colors = new Color32[width * height];
@@ -54,8 +60,47 @@ public class TextureTest : MonoBehaviour {
 		_texture.Apply();
 	}
 
+	Vector2 GetWorldPosition(int i, int j)
+	{
+		float x = (i - width / 2) * transform.localScale.x;
+		float y = (j - height / 2) * transform.localScale.y;
+
+		// float x = j * transform.localScale.x - width * transform.localScale.x * 0.5f;
+		// float y = (i * width) * transform.localScale.y - height * transform.localScale.y * 0.5f;
+
+		return new Vector2(x, y);
+	}
+
+	void UpdateVision()
+	{
+		var greyColor = new Color32(128, 0, 0, 0);
+		var whiteColor = new Color32(255, 255, 255, 255);
+
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				var position = GetWorldPosition(j, i);
+
+				foreach (var vision in _visions)
+				{
+					if (vision.InRange(position.x, position.y)) {
+						_colors[(i * width) + j] = whiteColor;				
+						break;
+					}
+						
+				}
+			}
+		}
+
+		_texture.SetPixels32(_colors);
+		_texture.Apply();
+	}
+
 	void FixedUpdate() {
-		RegenerateColors(vars[0], vars[1], vars[2], vars[3], vars[4]);
+		// RegenerateColors(vars[0], vars[1], vars[2], vars[3], vars[4]);
+		if (testVision)
+			UpdateVision();
 	}
 
 }
---
+++ FogTests/Assets/Scenes/Vision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour {
+
+	public float range = 100;
+
+	public bool InRange(float x, float y) {
+		return Vector2.Distance(new Vector2(x, y), transform.position) < range;
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position, range);	
+	}
+	
+}
