9b0c70e196df49a28543ccda4a17111436b6bd06
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-30 23:27:07 -0300

    changed to use color to void gc

--- FogTests/Assets/Scenes/TextureTest.cs
+++ FogTests/Assets/Scenes/TextureTest.cs
@@ -9,9 +9,9 @@ public class TextureTest : MonoBehaviour {
 	public int width = 128;
 	public int height = 128;
 
-	Color32[] _colors;
+	Color[] _colors;
 
-	public int[] vars = new int[5];
+	// public int[] vars = new int[5];
 
 	Texture2D _texture;
 
@@ -23,40 +23,30 @@ public class TextureTest : MonoBehaviour {
     {
 		_visions = FindObjectsOfType<Vision>();
 
-		_texture =  new Texture2D(width, height, TextureFormat.RGBA32, false, true);
+		_texture =  new Texture2D(width, height, TextureFormat.RGBA32, false, false);
+		_texture.filterMode = FilterMode.Point;
+		_texture.wrapMode = TextureWrapMode.Clamp;
 
-		_colors = new Color32[width * height];
-		RegenerateColors(vars[0], vars[1], vars[2], vars[3], vars[4]);
+		_colors = new Color[width * height];
+		RegenerateColors();
 
 		spriteRenderer.sprite = Sprite.Create(_texture, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f), 1);
     }
 
-	void RegenerateColors(int x, int y, int w, int h, int border)
+	void RegenerateColors()
 	{
-		var blackColor = new Color32(0, 0, 0, 255);
-		var greyColor = new Color32(128, 0, 0, 0);
-		var whiteColor = new Color32(255, 255, 255, 255);
-
-		int minx = x - w;
-		int maxx = x + w;
-
-		int miny = y - h;
-		int maxy = y + h;
+		var blackColor = new Color(0, 0, 0, 1.0f);
 
 		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
 			{
 				var color = blackColor;
-				if (i > miny && i < maxy && j > minx  && j < maxx)
-					color = greyColor;
-				if (i > miny + border && i < maxy - border && j > minx + border && j < maxx - border)
-					color = whiteColor;
-				_colors[(i * width) + j] = color;				
+				_colors[(i * width) + j] = blackColor;				
 			}
 		}
 
-		_texture.SetPixels32(_colors);
+		_texture.SetPixels(_colors);
 		_texture.Apply();
 	}
 
@@ -76,8 +66,8 @@ public class TextureTest : MonoBehaviour {
 
 	void UpdateVision()
 	{
-		var greyColor = new Color32(128, 0, 0, 0);
-		var whiteColor = new Color32(255, 255, 255, 255);
+		var greyColor = new Color(0.5f, 0, 0, 1.0f);
+		var whiteColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
 		for (int i = 0; i < height; i++)
 		{
@@ -99,13 +89,13 @@ public class TextureTest : MonoBehaviour {
 				if (!visionFound) {
 					var currentColor = _colors[(i * width) + j];
 
-					if (currentColor.Equals(whiteColor)) 
+					if (currentColor == whiteColor) 
 						_colors[(i * width) + j] = greyColor;
 				}
 			}
 		}
 
-		_texture.SetPixels32(_colors);
+		_texture.SetPixels(_colors);
 		_texture.Apply();
 	}
 
