5d88abe6dc920edf284bb6c313127fe56965e7d8
Ariel Coppes <ariel.coppes@gmail.com>
2018-01-31 22:54:53 -0300

    started tests on render texture, added bw shader from web example

---
+++ FogTests/Assets/Scenes/OtherScene/BlackAndWhiteEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackAndWhiteEffect : MonoBehaviour
+{
+    public float intensity;
+    private Material material;
+
+	public Shader shader;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(shader);
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, material);
+    }
+}
