45b2cee3ce7cbd9a3384620f8790da30b264dbfb
Ariel Coppes <ariel.coppes@gmail.com>
2018-06-08 21:45:30 -0300

    just testing with having a system for spawner behaviour

---
+++ unity-ecs1/Assets/VirtualVillagers/Components/SpawnerComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VirtualVillagers.Components
+{
+    public class SpawnerComponent : MonoBehaviour
+    {
+        public float totalIdleTime;
+        public float currentIdleTime;
+        public float max;
+        public Bounds bounds;
+        public GameObject prefab;
+
+        public int _debugCurrentSpawned;
+    }
+}
---
+++ unity-ecs1/Assets/VirtualVillagers/Systems/SpawnerSystem.cs
@@ -0,0 +1,55 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using VirtualVillagers.Components;
+
+namespace VirtualVillagers.Systems
+{
+    public class SpawnerSystem : ComponentSystem
+    {
+        private struct Data
+        {
+            public int Length;
+            public ComponentArray<SpawnerComponent> spawners;
+        }
+
+        [Inject] private Data _data;
+        
+        protected override void OnUpdate()
+        {
+            var dt = Time.deltaTime;
+            
+            for (var i = 0; i < _data.Length; i++)
+            {
+                var spawner = _data.spawners[i];
+
+                if (spawner.prefab == null)
+                    continue;
+                
+                if (spawner.currentIdleTime + dt < spawner.totalIdleTime)
+                {
+                    spawner.currentIdleTime += dt;
+                    continue;
+                }
+                                
+                if (string.IsNullOrEmpty(spawner.prefab.tag))
+                    continue;
+
+                var spawnedEntities = GameObject.FindGameObjectsWithTag(spawner.prefab.tag);
+
+                spawner._debugCurrentSpawned = spawnedEntities.Length;
+
+                if (spawnedEntities.Length >= spawner.max)
+                    continue;
+                
+                var x = UnityEngine.Random.Range(spawner.bounds.min.x, spawner.bounds.max.x);
+                var y = UnityEngine.Random.Range(spawner.bounds.min.y, spawner.bounds.max.y);
+                
+                var spawnItem = GameObject.Instantiate(spawner.prefab);
+
+                spawnItem.transform.position = new Vector3(x, y, 0);
+                spawner.currentIdleTime = spawner.currentIdleTime + dt - spawner.totalIdleTime;
+            }
+           
+        }
+    }
+}
