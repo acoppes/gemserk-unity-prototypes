e9adf7be119fd506f219121adb6ed0190a839766
Ariel Coppes <ariel.coppes@gmail.com>
2018-07-22 11:54:24 -0300

    playing with custom editor and waypoints

---
+++ FogTests/Assets/Plugins/Waypoints/Editor/WaypointInspector.cs
@@ -0,0 +1,83 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Waypoint)), CanEditMultipleObjects]
+public class WaypointInspector : Editor {
+
+    protected virtual void OnSceneGUI()
+    {
+        var waypoint = target as Waypoint;
+
+//        EditorGUI.BeginChangeCheck();
+//        var newTargetPosition = Handles.PositionHandle(waypoint.targetPosition, Quaternion.identity);
+//        if (EditorGUI.EndChangeCheck())
+//        {
+//            Undo.RecordObject(waypoint, "Change Look At Target Position");
+//            waypoint.targetPosition = newTargetPosition;
+//            waypoint.Update();
+//        }
+        
+////        if (Event.current.shift)
+////        {
+//            EditorGUI.BeginChangeCheck();
+//            var t = Handles.FreeMoveHandle(waypoint.transform.position, Quaternion.identity,
+//                HandleUtility.GetHandleSize(Vector3.zero) * 0.1f, waypoint.transform.position, Handles.DotHandleCap);
+////        Handles.ScaleHandle(HandleUtility.GetHandleSize(waypoint.transform.position));
+//            if (EditorGUI.EndChangeCheck())
+//            {
+//                waypoint.transform.position = t;
+//            }
+////        }
+
+        var wposition = waypoint.transform;
+
+        var points = waypoint.points;
+        for (var i = 0; i < points.Count; i++)
+        {
+            var point = wposition.TransformPoint(points[i]);
+            if (waypoint.showNames)
+                Handles.Label(point + waypoint.nameOffset, string.Format("{0}[{1}]", waypoint.name, i));
+            EditorGUI.BeginChangeCheck();
+            var newPointPosition = Handles.FreeMoveHandle(point, Quaternion.identity,
+                HandleUtility.GetHandleSize(Vector3.zero) * 0.1f, point, Handles.DotHandleCap);
+            if (EditorGUI.EndChangeCheck())
+            {
+                waypoint.points[i] = wposition.InverseTransformPoint(newPointPosition);
+            }
+        }
+
+        for (var i = 0; i < points.Count; i++)
+        {
+            var p0 = wposition.TransformPoint(points[i]);
+            var next = i + 1;
+            
+            if (next >= points.Count)
+            {
+                if (!waypoint.loop)
+                    break;
+                next = 0;
+            }
+            
+            var p1 = wposition.TransformPoint(points[next]);
+            
+            Handles.DrawLine(p0, p1);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var waypoint = target as Waypoint;
+//        waypoint.Update();
+
+        if (GUILayout.Button("Add"))
+        {
+            var newPoint = new Vector3();
+            if (waypoint.points.Count > 0)
+            {
+                newPoint = waypoint.points[waypoint.points.Count - 1];
+            }
+            waypoint.points.Add(newPoint + waypoint.newWaypointOffset);
+        }
+    }
+}
---
+++ FogTests/Assets/Plugins/Waypoints/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+//	public Vector3 targetPosition { get { return m_TargetPosition; } set { m_TargetPosition = value; } }
+//	[SerializeField]
+//	private Vector3 m_TargetPosition = new Vector3(1f, 0f, 2f);
+
+	public bool showNames = true;
+	public bool loop = true;
+	
+	public Vector3 nameOffset;
+	public Vector3 newWaypointOffset;
+	
+	public List<Vector3> points = new List<Vector3>();
+	
+//	public virtual void Update()
+//	{
+//		transform.LookAt(m_TargetPosition);
+//	}
+}
