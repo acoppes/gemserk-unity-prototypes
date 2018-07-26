a7bf9f04f05d7092022828be0386ae6050b09941
Ariel Coppes <ariel.coppes@gmail.com>
2018-07-25 23:45:53 -0300

    more nicer scene, added button to switch visions

---
+++ FogTests/Assets/Scenes/MainScene/Debug/DebugScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+	[SerializeField]
+	private VisionSystem _visionSystem;
+
+	public void SwitchPlayerVision()
+	{
+		_visionSystem.currentPlayer = (_visionSystem.currentPlayer + 1) % _visionSystem.totalPlayers;
+	}	
+}
