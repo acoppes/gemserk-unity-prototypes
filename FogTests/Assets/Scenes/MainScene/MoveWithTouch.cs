7c163d6df940edd316545feef503de7a7087b3bd
Ariel Coppes <ariel.coppes@gmail.com>
2018-07-25 23:52:42 -0300

    move with touch fog moves between ground levels

--- FogTests/Assets/Scenes/MainScene/MoveWithTouch.cs
+++ FogTests/Assets/Scenes/MainScene/MoveWithTouch.cs
@@ -5,6 +5,9 @@ public class MoveWithTouch : MonoBehaviour
 	[SerializeField]
 	protected Camera _referenceCamera;
 
+	[SerializeField]
+	protected Vision _vision;
+
 	void Update () {
 
 		if (Input.GetMouseButton(0))
@@ -12,6 +15,19 @@ public class MoveWithTouch : MonoBehaviour
 			var v3 = Input.mousePosition;
 			v3 = _referenceCamera.ScreenToWorldPoint(v3);
 			transform.position = v3;
+
+			_vision.groundLevel = 0;
+			
+			var collider = Physics2D.OverlapPoint(v3);
+			if (collider != null)
+			{
+				var obstacle = collider.GetComponent<VisionObstacle>();
+				if (obstacle != null)
+				{
+					_vision.groundLevel = obstacle.groundLevel + 1;
+				}
+			}
+			
 		}
 	}
 }
