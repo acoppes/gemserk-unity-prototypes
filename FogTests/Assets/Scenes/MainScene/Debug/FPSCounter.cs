384c770fbe9f88722a8197f54621117454f7a1de
Ariel Coppes <ariel.coppes@gmail.com>
2018-07-30 20:13:09 -0300

    better fps counter

---
+++ FogTests/Assets/Scenes/MainScene/Debug/FPSCounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+
+    public int FPS { get; private set; }
+	
+    void Update () {
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+    }
+}
