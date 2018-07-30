47216eb3cbedc7102b11855794307fc6fed3c4d5
Ariel Coppes <ariel.coppes@gmail.com>
2018-07-29 22:13:00 -0300

    button to turn off fpstext

--- FogTests/Assets/Scenes/MainScene/Debug/DebugScript.cs
+++ FogTests/Assets/Scenes/MainScene/Debug/DebugScript.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class DebugScript : MonoBehaviour
 {
 	[SerializeField]
 	private VisionSystem _visionSystem;
 
+	[SerializeField]
+	private Text _fpsText;
+
 	public void SwitchPlayerVision()
 	{
 		_visionSystem.currentPlayer = (_visionSystem.currentPlayer + 1) % _visionSystem.totalPlayers;
-	}	
+	}
+
+	public void ToggleFpsText()
+	{
+		if (_fpsText != null)
+		{
+			_fpsText.gameObject.SetActive(!_fpsText.gameObject.active);
+		}
+	}
 }
