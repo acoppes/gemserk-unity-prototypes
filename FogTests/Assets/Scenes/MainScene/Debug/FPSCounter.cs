da4e6d09ae3463410eb2ead7e0f53f28ff2da7ed
Ariel Coppes <ariel.coppes@gmail.com>
2018-08-02 08:04:03 -0300

    added fps average (10 frames)

--- FogTests/Assets/Scenes/MainScene/Debug/FPSCounter.cs
+++ FogTests/Assets/Scenes/MainScene/Debug/FPSCounter.cs
@@ -1,11 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine;
 
-public class FPSCounter : MonoBehaviour {
-
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField]
+    protected int _averageFrameCount;
+    
     public int FPS { get; private set; }
-	
-    void Update () {
-        FPS = (int)(1f / Time.unscaledDeltaTime);
+
+    private float _averageAccumulatedTime;
+    private int _frames;
+
+    private void Update () {
+        if (_averageFrameCount > 0)
+        {
+            _averageAccumulatedTime += Time.unscaledDeltaTime;
+
+            _frames++;
+
+            if (_frames > _averageFrameCount)
+            {
+                var averageTime = _averageAccumulatedTime / _frames;
+                FPS = (int) (1f / averageTime);
+                _averageAccumulatedTime = 0.0f;
+                _frames = 0;
+            }
+        }            
+        else
+        {
+            FPS = (int)(1f / Time.unscaledDeltaTime);        
+        }
     }
 }
