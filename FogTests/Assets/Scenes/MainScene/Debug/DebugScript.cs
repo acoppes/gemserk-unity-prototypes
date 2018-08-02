a708508bc1703449bdda3dba483392f2a200463e
Ariel Coppes <ariel.coppes@gmail.com>
2018-08-01 21:04:59 -0300

    button to toggle fog internal texture in main camera

--- FogTests/Assets/Scenes/MainScene/Debug/DebugScript.cs
+++ FogTests/Assets/Scenes/MainScene/Debug/DebugScript.cs
@@ -9,6 +9,26 @@ public class DebugScript : MonoBehaviour
 	[SerializeField]
 	private Text _fpsText;
 
+	[SerializeField]
+	protected Camera _mainCamera;
+
+	[SerializeField]
+	protected string _fogLayerName = "Fog";
+	
+	public void ToggleFogTexture()
+	{
+		var fogMask = LayerMask.GetMask(_fogLayerName);
+
+		if ((_mainCamera.cullingMask & fogMask) == fogMask)
+		{
+			_mainCamera.cullingMask = _mainCamera.cullingMask & ~fogMask;
+		}
+		else
+		{
+			_mainCamera.cullingMask = _mainCamera.cullingMask | fogMask;
+		}
+	}
+
 	public void SwitchPlayerVision()
 	{
 		_visionSystem.currentPlayer = (_visionSystem.currentPlayer + 1) % _visionSystem.totalPlayers;
